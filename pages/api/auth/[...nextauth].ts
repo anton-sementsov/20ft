fb9b21ff59722b860bdd364ccc7cc3bd0c9743d9
Anton Sementsov <sementsov.a58@gmail.com>
2023-07-05 11:22:06 +0300

    Add credentials provider

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,19 +1,41 @@
 import { NextApiHandler } from "next";
 import NextAuth, { NextAuthOptions } from "next-auth";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import GitHubProvider from "next-auth/providers/github";
-import EmailProvider from "next-auth/providers/email";
+// import GitHubProvider from "next-auth/providers/github";
+// import EmailProvider from "next-auth/providers/email";
 import prisma from "../../../lib/prisma";
+import Credentials from "next-auth/providers/credentials";
 
 const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
 export default authHandler;
 
 const options: NextAuthOptions = {
   providers: [
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+    Credentials({    
+      name: 'Credentials',       
+      credentials: {      
+        email: { 
+          label: "Email", 
+          type: "email",
+          placeholder: "email@gmail.com" 
+        },      
+        password: {  
+          label: "Password", 
+          type: "password" 
+        }    
+      },    
+      async authorize(credentials, req) {          
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email },
+        });
+        // check pass too 
+        return user || null;
+      }  
     }),
+    // GitHubProvider({
+    //   clientId: process.env.GITHUB_ID,
+    //   clientSecret: process.env.GITHUB_SECRET,
+    // }),
     // EmailProvider({
     //   server: {
     //     host: process.env.SMTP_HOST,
@@ -26,6 +48,9 @@ const options: NextAuthOptions = {
     //   from: process.env.SMTP_FROM,
     // }),
   ],
+  session: {
+    maxAge: 1 * 60 * 60, // 1 hour
+  },
   adapter: PrismaAdapter(prisma),
   secret: process.env.SECRET,
 };
