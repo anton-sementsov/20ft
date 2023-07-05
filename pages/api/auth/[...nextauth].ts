7a37f6b81285f3939b5d624fd25fd6c4614787aa
Anton Sementsov <sementsov.a58@gmail.com>
2023-07-05 11:34:15 +0300

    Add eslint and prettier

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,36 +1,36 @@
-import { NextApiHandler } from "next";
-import NextAuth, { NextAuthOptions } from "next-auth";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { NextApiHandler } from 'next';
+import NextAuth, { NextAuthOptions } from 'next-auth';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
 // import GitHubProvider from "next-auth/providers/github";
 // import EmailProvider from "next-auth/providers/email";
-import prisma from "../../../lib/prisma";
-import Credentials from "next-auth/providers/credentials";
+import prisma from '../../../lib/prisma';
+import Credentials from 'next-auth/providers/credentials';
 
 const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
 export default authHandler;
 
 const options: NextAuthOptions = {
   providers: [
-    Credentials({    
-      name: 'Credentials',       
-      credentials: {      
-        email: { 
-          label: "Email", 
-          type: "email",
-          placeholder: "email@gmail.com" 
-        },      
-        password: {  
-          label: "Password", 
-          type: "password" 
-        }    
-      },    
-      async authorize(credentials, req) {          
+    Credentials({
+      name: 'Credentials',
+      credentials: {
+        email: {
+          label: 'Email',
+          type: 'email',
+          placeholder: 'email@gmail.com',
+        },
+        password: {
+          label: 'Password',
+          type: 'password',
+        },
+      },
+      async authorize(credentials, req) {
         const user = await prisma.user.findUnique({
           where: { email: credentials.email },
         });
-        // check pass too 
+        // check pass too
         return user || null;
-      }  
+      },
     }),
     // GitHubProvider({
     //   clientId: process.env.GITHUB_ID,
